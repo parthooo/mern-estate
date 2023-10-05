3f42a6c2b20faebe53585736c58466620dadc652
Partho+1-at-070822867684 <partho.rahman@sjinnovation.com>
2023-10-05 17:43:10 +0600

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { signin, signup } from '../controller/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signup);
+authRouter.post('/signin', signin);
 
 export default authRouter; 
