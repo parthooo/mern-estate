81863644e78c65208bd19f714f28a8ffee314321
Partho+1-at-070822867684 <partho.rahman@sjinnovation.com>
2023-10-05 17:10:16 +0600

    complete sign up page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
-    const {userName, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User({userName, email, password: hashedPassword});
-    try {
-        await newUser.save()
-        res.status(201).json("user created succesfully");
-    }catch (error){
-        next(error);
-    }
-}
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("user created succesfully");
+  } catch (error) {
+    next(error);
+  }
+};
